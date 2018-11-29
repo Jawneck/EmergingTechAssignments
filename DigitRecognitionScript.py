7aff4973678aa6709dc1ebd7e472396a44e27662
unknown <dananasas@gmail.com>
2018-11-29 01:10:24 +0000

    achieved 97.7% accuracy with 20 epochs

--- DigitRecognitionScript.py
+++ DigitRecognitionScript.py
@@ -9,13 +9,14 @@ import sklearn.preprocessing as pre
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
 
-#Starting a neural network, building it by layers.
-model = kr.models.Sequential()
 
-model.add(Dense(512, activation='relu', input_shape=(784,)))
-model.add(Dropout(0.2))
-model.add(Dense(512, activation='relu'))
-model.add(Dropout(0.2))
+#Starting a neural network, building it by layers.
+model = Sequential()
+
+model.add(Dense(512, activation='relu',kernel_initializer="normal", input_dim=784))
+model.add(Dropout(0.1))
+model.add(Dense(512, activation='relu',kernel_initializer="normal"))
+model.add(Dropout(0.1))
 model.add(Dense(units=10, activation='softmax'))
 
 #Building the graph.
@@ -30,8 +31,11 @@ with gzip.open('data/train-labels-idx1-ubyte.gz', 'rb') as f:
     train_lbl = f.read()
 
 #Reshape images and labels
-train_img = ~np.array(list(train_img[16:])).reshape(60000, 28, 28).astype(np.uint8) / 255.0
-train_lbl =  np.array(list(train_lbl[ 8:])).astype(np.uint8)
+train_img = ~np.array(list(train_img[16:])).reshape(60000, 1, 784).astype(np.float32) / 255.0
+train_lbl =  np.array(list(train_lbl[ 8:])).astype(np.float32)
+
+# convert class vectors to binary class matrices
+train_lbl = kr.utils.to_categorical(train_lbl)
 
 #Reshape image array
 inputs = train_img.reshape(60000, 784)
@@ -41,7 +45,7 @@ encoder.fit(train_lbl)
 outputs = encoder.transform(train_lbl)
 
 #Training the model
-model.fit(inputs, outputs, epochs=20, batch_size=100)
+model.fit(inputs, outputs, epochs=20, batch_size=128)
 
 
 
