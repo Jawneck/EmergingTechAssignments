5302706709d9a619110e93ca11f87a8baa6cbd6c
unknown <dananasas@gmail.com>
2018-11-29 01:40:22 +0000

    reading in test set

--- DigitRecognitionScript.py
+++ DigitRecognitionScript.py
@@ -30,9 +30,18 @@ with gzip.open('data/train-images-idx3-ubyte.gz', 'rb') as f:
 with gzip.open('data/train-labels-idx1-ubyte.gz', 'rb') as f:
     train_lbl = f.read()
 
+with gzip.open('data/t10k-images-idx3-ubyte.gz', 'rb') as f:
+    test_img = f.read()
+    
+with gzip.open('data/t10k-labels-idx1-ubyte.gz', 'rb') as f:
+    test_lbl = f.read()
+
 #Reshape images and labels
-train_img = ~np.array(list(train_img[16:])).reshape(60000, 1, 784).astype(np.float32) / 255.0
-train_lbl =  np.array(list(train_lbl[ 8:])).astype(np.float32)
+train_img = ~np.array(list(train_img[16:])).reshape(60000, 1, 784).astype(np.uint8) / 255.0
+train_lbl =  np.array(list(train_lbl[ 8:])).astype(np.uint8)
+
+test_img = ~np.array(list(test_img[16:])).reshape(10000, 784).astype(np.uint8) / 255.0
+test_lbl = np.array(list(test_lbl[8:])).astype(np.uint8)
 
 # convert class vectors to binary class matrices
 train_lbl = kr.utils.to_categorical(train_lbl)
